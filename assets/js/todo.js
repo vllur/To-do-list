fd574f290f3cd77e4a851c0ceed4046992b60100
vllur <veller950@gmail.com>
2020-06-08 20:12:01 +0200

    Code style unification

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function(){
 	var form = document.getElementById("Form");
 	var input = document.getElementById("Input");
 	var button = document.getElementById("Button");
@@ -15,15 +15,15 @@ window.onload = function() {
 	list.addEventListener("click", boxChecked);
 	buttonClear.addEventListener("click", clearList);
 
-	if(localStorage.length <= 0) {
+	if(localStorage.length <= 0){
 		buttonClear.style.display = "none";
 	}
-	if(localStorage.length > 0) {
+	if(localStorage.length > 0){
 		displayList();
 	}
 
-	function addTodoItem() {
-		if(input.value === "") {
+	function addTodoItem(){
+		if(input.value === ""){
 			alert("You must enter some value!");
 		}
 		else {
@@ -40,9 +40,9 @@ window.onload = function() {
 	}
 
 	//adding string through style to list itme
-	function boxChecked(event) {
+	function boxChecked(event){
 		const element = event.target;
-		if(element.type === "checkbox") {
+		if(element.type === "checkbox"){
 			element.parentNode.style.textDecoration = "line-through";
 			todoList = JSON.parse(localStorage.getItem("todoList"));
 			todoList[element.id.split('-')[1]-1].checked = element.checked.toString();
@@ -50,8 +50,8 @@ window.onload = function() {
 		}
 	}
 
-	function addToLocalStorage() {
-		if(typeof(Storage) !== "undefined") {
+	function addToLocalStorage(){
+		if(typeof(Storage) !== "undefined"){
 			localStorage.setItem("todoList", JSON.stringify(todoList));
 		}
 		else {
@@ -60,15 +60,15 @@ window.onload = function() {
 	}
 
 	//display all todo list
-	function displayList() {
+	function displayList(){
 		todoList = JSON.parse(localStorage.getItem("todoList"));
-		todoList.forEach(function(element) {
+		todoList.forEach(function(element){
 			console.log(element.item)
 			var text = element.item;
 			var item = `<li id="li-${id}"><div class="text">${text}</div><input id="box-${id}" type="checkbox"></li>`;
 			list.insertAdjacentHTML("beforeend", item);
 			//if we got a checked box, then style
-			if(element.checked) {
+			if(element.checked){
 				var li = document.getElementById("li-"+id);
 				li.style.textDecoration = "line-through";
 				li.childNodes[1].checked = element.checked;
@@ -78,7 +78,7 @@ window.onload = function() {
 	}
 
 	//clear list event listener
-	function clearList() {
+	function clearList(){
 		todoList = [];
 		localStorage.clear();
 		list.innerHTML = "";
