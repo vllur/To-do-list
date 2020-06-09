62fa4adeda537e3f8484bf0fdfd2c3f23a594d99
vllur <veller950@gmail.com>
2020-06-09 15:53:33 +0200

    Added note deleting; added Fontello icons

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # To-do-list
 To do list web application.
+
+### Font license info
+- Font Awesome - Copyright (C) 2016 by Dave Gandy
+- Elusive - Copyright (C) 2013 by Aristeides Stathopoulos
--- assets/js/todo.js
+++ assets/js/todo.js
@@ -11,6 +11,7 @@ window.onload = function(){
 
 	button.addEventListener("click", addTodoItem);
 	list.addEventListener("click", boxChecked);
+	list.addEventListener("click", deleteItem);
 	buttonClear.addEventListener("click", clearList);
 
 	input.addEventListener("keyup", function(event){
@@ -31,7 +32,7 @@ window.onload = function(){
 		}else{
 			buttonClear.style.display = "inline";
 			var text = input.value;
-			var item = `<li id="li-${id}"><div class="text">${text}</div><input id="box-${id}" type="checkbox"></li>`;
+			var item = `<li id="li-${id}"><div class="text">${text}</div><div class="controls"><i id="del-${id}" class="icon-trash"></i><input id="box-${id}" type="checkbox"></div></li>`;
 			list.insertAdjacentHTML('beforeend', item);
 			liItem = {item: text, checked: false};
 			todoList.push(liItem);
@@ -66,18 +67,39 @@ window.onload = function(){
 		}
 	}
 
+	function deleteItem(event){
+		var id = Number(event.target.id[4]) - 1;
+		todoList = JSON.parse(localStorage.getItem("todoList"));
+		if(event.target.className == "icon-trash"){
+	    if(confirm("Do you really want to delete this note?")){
+	      todoList.splice(id, 1);
+	    }
+
+		localStorage.setItem("todoList", JSON.stringify(todoList));
+
+		if(!(Array.isArray(todoList) && todoList.length)){
+			todoList = [];
+  		localStorage.clear();
+  		buttonClear.style.display = "none";
+		}
+		list.innerHTML = "";
+		location.reload();
+		displayList();
+		}
+	}
+
 	function displayList(){
 		todoList = JSON.parse(localStorage.getItem("todoList"));
 
 		todoList.forEach(function(element){
   		var text = element.item;
-  		var item = `<li id="li-${id}"><div class="text">${text}</div><input id="box-${id}" type="checkbox"></li>`;
+  		var item = `<li id="li-${id}"><div class="text">${text}</div><div class="controls"><i id="del-${id}" class="icon-trash"></i><input id="box-${id}" type="checkbox"></div></li>`;
   		list.insertAdjacentHTML("beforeend", item);
 
   		if(element.checked == true){
   			var li = document.getElementById("li-"+id);
   			li.style.textDecoration = "line-through";
-  			li.childNodes[1].checked = true;
+  			li.childNodes[2].checked = true;
   		}
   		id++;
 		});
